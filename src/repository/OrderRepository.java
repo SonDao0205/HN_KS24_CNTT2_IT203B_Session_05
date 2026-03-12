5c84aec63b3f6866baed2203d4ccfbb3021bc6be
Dao Truong Son <daotruongson2006@gmail.com>
2026-03-12 08:01:47 +0700

    submit

--- src/repository/OrderRepository.java
+++ src/repository/OrderRepository.java
@@ -5,36 +5,34 @@ import model.OrderStatus;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 public class OrderRepository {
     public static List<Order> orders = new ArrayList<Order>();
 
-//    static {
-//        // Giả lập đơn hàng 1 (Đã thanh toán)
-//        Order o1 = new Order("ORD01");
-//        o1.addItem(MenuRepository.findItemById("F01"), 2);
-//        o1.setStatus(OrderStatus.PAID);
-//        orders.add(o1);
-//
-//        // Giả lập đơn hàng 2 (Đã thanh toán)
-//        Order o2 = new Order("ORD02");
-//        o2.addItem(MenuRepository.findItemById("D01"), 3); // 3 Trà sữa
-//        o2.setStatus(OrderStatus.PAID);
-//        orders.add(o2);
-//
-//        // Giả lập đơn hàng 3 (Đang chờ)
-//        Order o3 = new Order("ORD03");
-//        o3.addItem(MenuRepository.findItemById("S01"), 1);
-//        o3.setStatus(OrderStatus.PENDING);
-//        orders.add(o3);
-//    }
+    static {
+        // Giả lập đơn hàng 1 (Đã thanh toán)
+        Order o1 = new Order("ORD01");
+        o1.addItem(MenuRepository.findItemById("F01"), 2);
+        o1.setStatus(OrderStatus.PAID);
+        orders.add(o1);
 
-    public static Order findOrderById(String id) {
-        for (Order order : orders) {
-            if (order.getId().equals(id)) {
-                return order;
-            }
-        }
-        return null;
+        // Giả lập đơn hàng 2 (Đã thanh toán)
+        Order o2 = new Order("ORD02");
+        o2.addItem(MenuRepository.findItemById("D01"), 3); // 3 Trà sữa
+        o2.setStatus(OrderStatus.PAID);
+        orders.add(o2);
+
+        // Giả lập đơn hàng 3 (Đang chờ)
+        Order o3 = new Order("ORD03");
+        o3.addItem(MenuRepository.findItemById("S01"), 1);
+        o3.setStatus(OrderStatus.PENDING);
+        orders.add(o3);
+    }
+
+    public static Optional<Order> findOrderById(String id) {
+        return orders.stream()
+                .filter(order -> order.getId().equals(id))
+                .findFirst();
     }
 }
--- src/service/OrderManagement.java
+++ src/service/OrderManagement.java
@@ -10,6 +10,7 @@ import repository.OrderRepository;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 
 import static repository.MenuRepository.menuItems;
 
@@ -22,7 +23,7 @@ public class OrderManagement implements IOrderService{
         if (order == null) {
             throw new IllegalArgumentException("Dữ liệu đơn hàng không hợp lệ");
         }
-        if(OrderRepository.findOrderById(order.getId()) != null){
+        if(OrderRepository.findOrderById(order.getId()).isPresent()) {
             System.out.println("Id đơn hàng đã tồn tại!");
             return;
         }
@@ -32,44 +33,50 @@ public class OrderManagement implements IOrderService{
 
     @Override
     public void updateStatus(String id, OrderStatus orderStatus) throws InvalidOrderIdException {
-        Order order = OrderRepository.findOrderById(id);
-        if (order == null) {
+        Optional<Order> order = OrderRepository.findOrderById(id);
+        if (order.isPresent()) {
+            order.get().setStatus(orderStatus);
+            System.out.println("Cập nhật trạng thái thành công!");
+        }else{
             throw new InvalidOrderIdException("Không tìm thấy đơn hàng ID: " + id);
         }
-        order.setStatus(orderStatus);
-        System.out.println("Cập nhật trạng thái thành công!");
+
     }
 
     @Override
     public void addItem(String orderId, String itemId, int quantity) throws InvalidOrderIdException {
-        Order order = OrderRepository.findOrderById(orderId);
+        Optional<Order> order = OrderRepository.findOrderById(orderId);
 
-        if (order == null) {
+        if (order.isPresent()) {
+            MenuItem item = MenuRepository.findItemById(itemId);
+            if (item == null) {
+                System.out.println("Không tìm thấy sản phẩm");
+                return;
+            }
+            order.get().addItem(item, quantity);
+            System.out.println("Thêm món thành công!");
+        }else{
             throw new InvalidOrderIdException("Không tìm thấy đơn hàng ID: " + orderId);
         }
-        MenuItem item = MenuRepository.findItemById(itemId);
-        if (item == null) {
-            System.out.println("Không tìm thấy sản phẩm");
-            return;
-        }
-        order.addItem(item, quantity);
-        System.out.println("Thêm món thành công!");
+
     }
 
     @Override
     public void displayOrder(String orderId) throws InvalidOrderIdException {
-        Order order = OrderRepository.findOrderById(orderId);
+        Optional<Order> order = OrderRepository.findOrderById(orderId);
 
-        if (order == null) {
+        if(order.isPresent()) {
+            System.out.println("Đơn hàng : ");
+            System.out.println(order.get());
+            System.out.println("Danh sách sản phẩm trong đơn hàng : ");
+            Map<MenuItem, Integer> items = order.get().getItems();
+            for (Map.Entry<MenuItem, Integer> entry : items.entrySet()) {
+                MenuItem item = entry.getKey();
+                System.out.println(item + " Số lượng : " + entry.getValue());
+            }
+        }else{
             throw new InvalidOrderIdException("Không tìm thấy đơn hàng ID: " + orderId);
         }
-        System.out.println("Đơn hàng : ");
-        System.out.println(order);
-        System.out.println("Danh sách sản phẩm trong đơn hàng : ");
-        Map<MenuItem, Integer> items = order.getItems();
-        for (Map.Entry<MenuItem, Integer> entry : items.entrySet()) {
-            MenuItem item = entry.getKey();
-            System.out.println(item + " Số lượng : " + entry.getValue());
-        }
+
     }
 }
